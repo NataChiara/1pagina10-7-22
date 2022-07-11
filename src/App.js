debe26e7540dbf84fc8a94f8cf42d632b34a6888
NataChiara <natachiara@outlook.com>
2022-07-10 21:08:04 -0300

    Creo la pagina

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import App from './routes';
 import './App.css';
+import Encabezado from './component/Header';
+import PieDePagina from './component/Footer';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Encabezado/>
+      <p>Nada</p>
+      <PieDePagina/>
     </div>
   );
 }
